c3b72e545031ad00d3cdfab3fb6ec727480f1db9
Josias-Ribeiro <josias_ribeiro90@hotmail.com>
2022-07-07 20:53:54 -0300

    BS - Estrutura do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { BookStoreAppComponent } from './components/book-store-app/book-store-app.component';
+import { FiltersComponent } from './components/book-store-app/filters/filters.component';
+import { ProductListComponent } from './components/book-store-app/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    BookStoreAppComponent,
+    FiltersComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
